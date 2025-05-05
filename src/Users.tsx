8e51b52245406edb21e178e345e210c96c4cc514
arthur <arthur.rios007@gmail.com>
2025-05-05 18:12:08 -0300

    feat: test loading and fetching

--- src/Users.tsx
+++ src/Users.tsx
@@ -1,16 +1,29 @@
 import { useQuery } from '@tanstack/react-query'
 import type { IUser } from './types'
+import { sleep } from './sleep'
 
 export function Users() {
-  const { data } = useQuery({
+  const { data, isLoading, refetch, isFetching } = useQuery({
+    enabled: false,
     queryKey: ['users'],
     queryFn: async (): Promise<IUser[]> => {
+      await sleep()
       const response = await fetch('http://localhost:3000/users')
       return response.json()
     },
   })
   return (
     <div>
+      <button
+        type="button"
+        className="bg-white text-black px-4 py-2 rounded-lg"
+        onClick={() => refetch()}
+      >
+        List users
+      </button>
+
+      {isLoading && <h1>'Loading...'</h1>}
+      {!isLoading && isFetching && <h1>'Fetching...'</h1>}
       {data?.map((user) => (
         <div key={user.id}>
           <strong className="block">{user.name}</strong>
