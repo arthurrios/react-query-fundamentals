67b66c7a9567cb92c3dfddbc172621ffbfb2de86
arthur <arthur.rios007@gmail.com>
2025-05-05 18:55:27 -0300

    feat: prefetch

--- src/Posts.tsx
+++ src/Posts.tsx
@@ -1,17 +1,30 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQueryClient } from '@tanstack/react-query'
 import type { IUser } from './types'
 import { sleep } from './sleep'
+import { Link } from 'react-router-dom'
 
 export function Posts() {
-  // const { data } = useQuery({
-  //   enabled: false,
-  //   queryKey: ['users'],
-  //   staleTime: 5000,
-  //   queryFn: async (): Promise<IUser[]> => {
-  //     await sleep()
-  //     const response = await fetch('http://localhost:3000/users')
-  //     return response.json()
-  //   },
-  // })
-  return <pre>{JSON.stringify({}, null, 2)}</pre>
+  const queryClient = useQueryClient()
+
+  function handleMouseEnter() {
+    queryClient.prefetchQuery({
+      queryKey: ['users'],
+      queryFn: async (): Promise<IUser[]> => {
+        // throw new Error('Error')
+        await sleep()
+        const response = await fetch('http://localhost:3000/users')
+        return response.json()
+      },
+    })
+  }
+
+  return (
+    <pre>
+      Posts
+      <br />
+      <Link to="/" onMouseEnter={handleMouseEnter}>
+        Go to users
+      </Link>
+    </pre>
+  )
 }
